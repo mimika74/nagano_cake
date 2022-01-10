98e1fe0b295af731e60c5d8f3687e4d3f70c07fe
mimika74 <88548032+mimika74@users.noreply.github.com>
2022-01-10 05:24:55 +0000

    [add]address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,9 +1,23 @@
 class Public::AddressesController < ApplicationController
 
   def index
+    @customer_address = current_customer.address
+    #@address1 = Address.order(updated_at: :desc).limit(1)
+    @addresses = Address.all
+    @address = Address.new()
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+
+    if  @address.save!
+      flash[:notice] = '商品が追加されました。'
+      redirect_to addresses_path
+    else
+      flash[:alert] = '商品の追加に失敗しました。'
+      redirect_to addresses_path
+    end
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2021_12_12_121203) do
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
     t.string "name"
-    t.string "postal_code"
-    t.string "address"
+    t.string "postal_code", :default => ''
+    t.string "address", :default => ''
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
