e19955043b400b1490e1553cccb87815e8adbca1
mimika74 <88548032+mimika74@users.noreply.github.com>
2022-02-06 10:01:45 +0000

    [add]layouts

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,16 @@ class Public::HomesController < ApplicationController
 
 
  def top
-  render 'items/index'
+    @genres = Genre.all
+    @items = Item.all.page(params[:page]).per(4)
+    @items = Item.where(genre_id: params[:genre_id].to_i) if params[:genre_id].present?
+    @search = Item.ransack(params[:q])
+    @search_items = @search.result
+    #@search_genres = @item.genre
+    #@search = Item.where(genre_id: params[:genre_id].to_i) if params[:genre_id].present?
+    @search_items = Item.where(genre_id: params[:genre_id].to_i) if params[:genre_id].present?
+
+
  end
 
  def about
